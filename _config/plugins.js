7971edb7f2392b8586ef548042bc17004ab5c7bb
Greg Walker <mgwalker@gmail.com>
2023-07-12 18:06:58 -0500

    search tweaks

--- _config/plugins.js
+++ _config/plugins.js
@@ -1,8 +1,76 @@
+const fs = require("fs/promises");
 const purgeCssPlugin = require("eleventy-plugin-purgecss");
 const sassPlugin = require("@grimlink/eleventy-plugin-sass");
 const sass = require("sass");
 
+const wordsToRemoveFromIngredients = new Set([
+  "cup",
+  "ounce",
+  "tablespoon",
+  "teaspoon",
+  "peel",
+  "pound",
+  "mince",
+  "slice",
+  "and",
+  "or",
+  "in",
+  "the",
+  "into",
+  "cut",
+]);
+
+for (const remove of [...wordsToRemoveFromIngredients]) {
+  wordsToRemoveFromIngredients.add(`${remove}s`);
+  wordsToRemoveFromIngredients.add(`${remove}ed`);
+  wordsToRemoveFromIngredients.add(`${remove}d`);
+}
+
 module.exports = (config) => {
   config.addPlugin(purgeCssPlugin);
   config.addPlugin(sassPlugin, { sass });
+
+  config.addFilter("jsonify", async (a) => {
+    const {
+      title,
+      page: { inputPath, url },
+    } = a.data;
+
+    const recipe = await fs
+      .readFile(inputPath, {
+        encoding: "utf-8",
+      })
+      .then((lines) => lines.split("\n").map((line) => line.trim()));
+
+    const ingredientsStart = recipe.findIndex((line) =>
+      /^##\s+Ingredients?/gi.test(line)
+    );
+
+    const ingredientsEnd = recipe.findIndex(
+      (line, index) => index > ingredientsStart && /^##\s+/gi.test(line)
+    );
+
+    const ingredients = new Set(
+      recipe
+        .slice(ingredientsStart, ingredientsEnd)
+        .filter((line) => /^[-*]\s+/.test(line))
+        .map((line) =>
+          line
+            .replace(/^[-*]\s+/g, "")
+            .replace(/\d+\/\d+"?/g, "")
+            .replace(/\d+-\d+"?/g, "")
+            .replace(/\d+-[^\s]+/g, "")
+            .replace(/\d+"/g, "")
+            .replace(/\d/g, "")
+            .replace(/[.(),;"]/gi, "")
+            .toLowerCase()
+            .split(" ")
+            .filter((v) => v.trim().length > 0)
+            .filter((v) => !wordsToRemoveFromIngredients.has(v))
+        )
+        .flat()
+    );
+
+    return `["${title}", "${url}", ${JSON.stringify([...ingredients])}]`;
+  });
 };
--- _js/main.js
+++ _js/main.js
@@ -3,14 +3,25 @@ const recipesPromise = fetch("/recipes.json").then((r) => r.json());
 document.addEventListener("DOMContentLoaded", async () => {
   const recipes = await recipesPromise;
 
-  const words = new Map();
-  for (const [name, path] of recipes) {
+  const titleWords = new Map();
+  const ingredients = new Map();
+
+  for (const [name, path, myIngredients] of recipes) {
+    const recipeReference = { name, url: path };
+
     const nameWords = name.toLowerCase().split(" ");
     for (const word of nameWords) {
-      if (!words.has(word)) {
-        words.set(word, []);
+      if (!titleWords.has(word)) {
+        titleWords.set(word, []);
       }
-      words.get(word).push([name, path]);
+      titleWords.get(word).push(recipeReference);
+    }
+
+    for (const word of myIngredients) {
+      if (!ingredients.has(word)) {
+        ingredients.set(word, []);
+      }
+      ingredients.get(word).push(recipeReference);
     }
   }
 
@@ -23,35 +34,88 @@ document.addEventListener("DOMContentLoaded", async () => {
         .split(" ")
         .filter((l) => l.length > 0);
 
-      const matches = new Map();
+      const titleMatches = new Map();
+      const ingredientMatches = new Map();
 
       for (const word of searchWords) {
-        words.forEach((recipes, key) => {
+        titleWords.forEach((recipes, key) => {
           if (key.includes(word)) {
-            recipes.forEach(([name, url]) => {
-              matches.set(name, url);
+            recipes.forEach(({ name, url }) => {
+              titleMatches.set(name, url);
             });
           }
         });
+
+        // ingredients.forEach((recipes, key) => {
+        //   if (key.includes(word)) {
+        //     recipes.forEach(({ name, url }) => {
+        //       if (ingredientMatches.has(name)) {
+        //         ingredientMatches.get(name).words.add(key);
+        //       } else {
+        //         ingredientMatches.set(name, { url, words: new Set([key]) });
+        //       }
+        //     });
+        //   }
+        // });
       }
 
-      if (matches.size > 0) {
-        document
-          .querySelector("main")
-          .setAttribute("data-search-active", "true");
-        const searchResults = document.getElementById("search-results");
+      const lines = [];
 
-        const results = [...matches.keys()].sort();
-        const lines = ["<ul>"];
+      if (titleMatches.size > 0) {
+        const results = [...titleMatches.keys()].sort();
+        lines.push("<h3>By title</h3>");
+        lines.push("<ul>");
         results.forEach((name) => {
-          lines.push(`  <li><a href="${matches.get(name)}">${name}</a></li>`);
+          lines.push(
+            `  <li><a href="${titleMatches.get(name)}">${name}</a></li>`
+          );
         });
-
-        searchResults.innerHTML = lines.join("\n");
-      } else {
-        document
-          .querySelector("main")
-          .setAttribute("data-search-active", "false");
+        lines.push("</ul>");
       }
+
+      for (const [recipeName, { words }] of ingredientMatches) {
+        const matchWords = [...words];
+
+        const keep = searchWords.every((searchWord) =>
+          matchWords.some((matchWord) => matchWord.includes(searchWord))
+        );
+        if (!keep) {
+          ingredientMatches.delete(recipeName);
+        }
+      }
+      if (ingredientMatches.size > 20) {
+        ingredientMatches.clear();
+      }
+
+      if (ingredientMatches.size > 0) {
+        const results = [...ingredientMatches.keys()].sort(
+          (a, b) =>
+            ingredientMatches.get(b).words.size -
+            ingredientMatches.get(a).words.size
+        );
+
+        lines.push("<h3>By ingredients</h3>");
+        lines.push("<ul>");
+        results.forEach((name) => {
+          const { url, words } = ingredientMatches.get(name);
+          lines.push(
+            `  <li><a href="${url}">${name}</a><div>${[...words]
+              .map((word) => `<ld-badge>${word}</ld-badge>`)
+              .join("")}</div></li>`
+          );
+        });
+        lines.push("</ul>");
+      }
+
+      if (lines.length) {
+        const searchResults = document.getElementById("search-results");
+        searchResults.innerHTML = lines.join("\n");
+      }
+
+      const showResults = titleMatches.size > 0 || ingredientMatches.size > 0;
+
+      document
+        .querySelector("main")
+        .setAttribute("data-search-active", `${showResults}`);
     });
 });
